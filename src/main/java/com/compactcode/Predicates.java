3e50ee0f54cc90059700baf2a26b0f83cf53b2e4
compactcode <shanonmcquay@gmail.com>
2010-04-19 14:29:36 +1000

    Hamcrest dependancy added to provide a standard library of predicates

--- src/main/java/com/compactcode/Predicates.java
+++ src/main/java/com/compactcode/Predicates.java
@@ -1,13 +1,20 @@
 package com.compactcode;
 
+import org.hamcrest.Matcher;
+import org.hamcrest.number.OrderingComparisons;
+
 import com.google.common.base.Predicate;
 
 public final class Predicates {
 
 	public static final Predicate<Integer> greaterThan(final int target) {
-		return new Predicate<Integer>() {
-			public boolean apply(Integer value) {
-				return value > target;
+		return fromMatcher(OrderingComparisons.greaterThan(target));
+	}
+	
+	public static final <T> Predicate<T> fromMatcher(final Matcher<T> matcher) {
+		return new Predicate<T>() {
+			public boolean apply(T element) {
+				return matcher.matches(element);
 			}
 		};
 	}
