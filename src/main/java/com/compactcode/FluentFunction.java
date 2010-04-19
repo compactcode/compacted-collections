8edef2bf1267dc63970c7b6e9197e629adcbc9f1
compactcode <shanonmcquay@gmail.com>
2010-04-19 18:20:28 +1000

    added javadocs to fluent predicate+function

--- src/main/java/com/compactcode/FluentFunction.java
+++ src/main/java/com/compactcode/FluentFunction.java
@@ -5,6 +5,12 @@ import com.google.common.base.Functions;
 import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
 
+
+/**
+ * Experimental.
+ * 
+ * Designed to make it easier to compose and chain functions together.
+ */
 public class FluentFunction<A, B> implements Function<A, B>{
 	
 	public static final <A, B> FluentFunction<A, B> function(Function<A, B> function) {
@@ -17,10 +23,16 @@ public class FluentFunction<A, B> implements Function<A, B>{
 		this.delegate = delegate;
 	}
 
+	/**
+	 * Compose this function with the given function using {@link Functions#compose(Function, Function)}.
+	 */
 	public <C> FluentFunction<A, C> map(Function<? super B, C> next) {
 		return function(Functions.compose(next, delegate));
 	}
 	
+	/**
+	 * Compose this function with the given predicate using {@link Predicates#compose(Predicate, Function)}.
+	 */
 	public FluentPredicate<A> check(Predicate<B> next) {
 		return FluentPredicate.predicate(Predicates.compose(next, this));
 	}
--- src/main/java/com/compactcode/FluentPredicate.java
+++ src/main/java/com/compactcode/FluentPredicate.java
@@ -3,6 +3,11 @@ package com.compactcode;
 import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
 
+/**
+ * Experimental.
+ * 
+ * Designed to make it easier to compose and chain predicates together.
+ */
 public class FluentPredicate<T> implements Predicate<T> {
 
 	public static final <T> FluentPredicate<T> predicate(Predicate<T> predicate) {
@@ -15,18 +20,30 @@ public class FluentPredicate<T> implements Predicate<T> {
 		this.delegate = delegate;
 	}
 
+	/**
+	 * And this predicate with the given predicate using {@link Predicates#and(Predicate, Predicate)}.
+	 */
 	public FluentPredicate<T> and(Predicate<T> other) {
 		return predicate(Predicates.<T>and(delegate, other));
 	}
 	
+	/**
+	 * And this predicate with the negation of the given predicate using {@link Predicates#and(Predicate, Predicate)}.
+	 */
 	public FluentPredicate<T> andNot(Predicate<T> other) {
 		return and(Predicates.not(other));
 	}
 	
+	/**
+	 * Or this predicate with the given predicate using {@link Predicates#or(Predicate, Predicate)}.
+	 */
 	public FluentPredicate<T> or(Predicate<T> other) {
 		return predicate(Predicates.<T>or(delegate, other));
 	}
 	
+	/**
+	 * Or this predicate with the negation of the given predicate using {@link Predicates#or(Predicate, Predicate)}.
+	 */
 	public FluentPredicate<T> orNot(Predicate<T> other) {
 		return or(Predicates.not(other));
 	}
