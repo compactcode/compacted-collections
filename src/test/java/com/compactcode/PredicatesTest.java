a200ae53e710e79d9d62ad5b17b6f342a82f5bc6
compactcode <shanonmcquay@gmail.com>
2010-04-20 17:45:35 +1000

    removed deprecated Predicates class

--- src/test/java/com/compactcode/PredicatesTest.java
+++ src/test/java/com/compactcode/PredicatesTest.java
@@ -1,9 +1,6 @@
 package com.compactcode;
 
-import static com.compactcode.FluentList.fluent;
 import static com.compactcode.FluentPredicate.predicate;
-import static com.compactcode.Predicates.greaterThan;
-import static com.google.common.collect.Lists.newArrayList;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
@@ -11,11 +8,6 @@ import org.junit.Test;
 import com.google.common.base.Predicates;
 
 public class PredicatesTest {
-
-	@Test
-	public void canFilterItemsGreaterThanValue() {
-		assertEquals(newArrayList(4, 5), fluent(newArrayList(1, 2, 3, 4, 5)).filter(greaterThan(3)));
-	}
 
 	@Test
 	public void canAndPredicatesFluently() {
